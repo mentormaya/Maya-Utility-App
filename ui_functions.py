fc0bb3673bb7d9d365fcc5eda2418114d6de4a45
Ajay Singh <ajay@mentormaya.com>
2022-07-14 17:00:51 +0545

    ui added

--- ui_functions.py
+++ ui_functions.py
@@ -11,31 +11,40 @@ class UI_Functions:
     
     #number utility functions
     def convertNumber(num, frame, _statusBar):
-        # print(num)
-        num = Number(num)
-        data = num.get_num()
-        frame.findChild(QLineEdit, "number_disp").setText(data["num"])
-        frame.findChild(QPushButton, "num_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["num"], _statusBar))
-        frame.findChild(QLineEdit, "english_disp").setText(data["english"])
-        frame.findChild(QPushButton, "eng_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["english"], _statusBar))
-        frame.findChild(QLineEdit, "nepali_disp").setText(data["nepali"])
-        frame.findChild(QPushButton, "nep_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["nepali"], _statusBar))
-        frame.findChild(QLineEdit, "decimal_disp").setText(data["decimal"])
-        frame.findChild(QPushButton, "decimal_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["decimal"], _statusBar))
-        frame.findChild(QLineEdit, "whole_disp").setText(data["whole"])
-        frame.findChild(QPushButton, "whole_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["whole"], _statusBar))
-        frame.findChild(QLineEdit, "lakh_format_disp").setText(data["lakh_format"])
-        frame.findChild(QPushButton, "lakh_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["lakh_format"], _statusBar))
-        frame.findChild(QLineEdit, "million_format_disp").setText(data["million_format"])
-        frame.findChild(QPushButton, "million_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["million_format"], _statusBar))
-        frame.findChild(QLabel, "words_eng_disp").setText(data["words_eng"])
-        frame.findChild(QPushButton, "eng_words_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["words_eng"], _statusBar))
-        frame.findChild(QLabel, "words_nep_disp").setText(data["words_nep"])
-        frame.findChild(QPushButton, "nep_words_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["words_nep"], _statusBar))
-        frame.findChild(QPushButton, "copy_num_all_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data, _statusBar))
-        status = f'{data["num"]} is converted!'
-        print(status)
-        _statusBar.setText(status)
+        if num == "":
+            _statusBar.setText("Enter the number to convert")
+            return
+        try:
+            num = float(num)
+        except ValueError:
+            _statusBar.setText("Please input the valid Number!")
+        except:
+            _statusBar.setText("Input Value Exception Occured!")
+        else:
+            num = Number(str(num))
+            data = num.get_num()
+            frame.findChild(QLineEdit, "number_disp").setText(data["num"])
+            frame.findChild(QPushButton, "num_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["num"], _statusBar))
+            frame.findChild(QLineEdit, "english_disp").setText(data["english"])
+            frame.findChild(QPushButton, "eng_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["english"], _statusBar))
+            frame.findChild(QLineEdit, "nepali_disp").setText(data["nepali"])
+            frame.findChild(QPushButton, "nep_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["nepali"], _statusBar))
+            frame.findChild(QLineEdit, "decimal_disp").setText(data["decimal"])
+            frame.findChild(QPushButton, "decimal_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["decimal"], _statusBar))
+            frame.findChild(QLineEdit, "whole_disp").setText(data["whole"])
+            frame.findChild(QPushButton, "whole_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["whole"], _statusBar))
+            frame.findChild(QLineEdit, "lakh_format_disp").setText(data["lakh_format"])
+            frame.findChild(QPushButton, "lakh_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["lakh_format"], _statusBar))
+            frame.findChild(QLineEdit, "million_format_disp").setText(data["million_format"])
+            frame.findChild(QPushButton, "million_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["million_format"], _statusBar))
+            frame.findChild(QLabel, "words_eng_disp").setText(data["words_eng"])
+            frame.findChild(QPushButton, "eng_words_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["words_eng"], _statusBar))
+            frame.findChild(QLabel, "words_nep_disp").setText(data["words_nep"])
+            frame.findChild(QPushButton, "nep_words_copy_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data["words_nep"], _statusBar))
+            frame.findChild(QPushButton, "copy_num_all_btn").clicked.connect(lambda: UI_Functions.copyToClipBoard(data, _statusBar))
+            status = f'{data["num"]} is converted!'
+            print(status)
+            _statusBar.setText(status)
     
     def clearText(_input, _statusBar):
         _input.setText("")
@@ -57,11 +66,11 @@ class UI_Functions:
         if not show:
             _menu.setMaximumWidth(SUB_MENU_MIN)
             _menu.setMinimumWidth(SUB_MENU_MIN)
-            print("Hiding Menu")
+            # print("Hiding Menu")
         else:
             _menu.setMaximumWidth(SUB_MENU_MAX)
             _menu.setMinimumWidth(SUB_MENU_MAX)
-            print("Showing Menu")
+            # print("Showing Menu")
             
     def showSubMenu(_index, _frame):
         _w = _frame.size().width()
@@ -69,14 +78,14 @@ class UI_Functions:
         _curr = _menu.currentIndex()
         if _curr == _index:
             UI_Functions.toggleMenu(_frame)
-            print("toggling menu!")
+            # print("toggling menu!")
         elif _w <= SUB_MENU_CHECK:
             UI_Functions.showMenu(_frame, True)
             _menu.setCurrentIndex(_index)
-            print(f"Showing menu and clicked! {_w}")
+            # print(f"Showing menu and clicked! {_w}")
         else:
             _menu.setCurrentIndex(_index)
-            print(f"menu clicked! {_w}")
+            # print(f"menu clicked! {_w}")
     
     def showPage(_widget, _container):
         _container.setCurrentWidget(_widget)
