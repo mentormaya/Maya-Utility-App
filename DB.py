ffc69025cbebdc12fc063f64a0b96f4bea9d20bc
Ajay Singh <ajay@mentormaya.com>
2022-07-22 11:14:17 +0545

    DB initialization added

--- DB.py
+++ DB.py
@@ -5,3 +5,18 @@ class DB():
         self.name = name
         self.connection = sqlite3.connect(self.name)
         self.cursor = self.connection.cursor()
+        # self.initTables()
+        
+    def __del__(self):
+        self.connection.commit()
+        self.connection.close()
+        
+    
+    def initTables(self):
+        query = """
+            CREATE TABLE IF NOT EXISTS Configs(
+                
+            )
+        """
+        print("Initializing Tables Structure")
+        print(query)
