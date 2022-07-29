7316c3e0ea7532d02e2ceb424d182865c37a6fd3
Ajay Singh <ajay@mentormaya.com>
2022-07-29 12:29:39 +0545

    updated

--- .qt_for_python/uic/DateConverter.py
+++ .qt_for_python/uic/DateConverter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\A00172\Desktop\New Utility\assets\UI\DateConverter.ui'
+# Form implementation generated from reading ui file 'd:\Creative\GitHub\Maya-Utility-App\assets\UI\DateConverter.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
