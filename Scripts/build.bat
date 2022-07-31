6c512f83828c593bda0a451dee497d7dfbc0c2a1
Ajay Singh <ajay@mentormaya.com>
2022-07-31 21:53:51 +0545

    bat script updated

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,2 +1,11 @@
 echo "Building the project..."
-pyinstaller --onefile --windowed --icon "assets//icon.ico" main.py
+pyinstaller ^
+    --onefile ^
+    --windowed ^
+    --icon "assets//icon.ico" ^
+    --name "MaYa_Utility" ^
+    --clean ^
+    --resource "assets" ^
+    --resource "uploads" ^
+    --resource ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv" ^
+    main.py
