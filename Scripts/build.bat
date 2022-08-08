aa8b6d6efa68746d960b9dc8791b85d2b0913385
Ajay Singh <ajay@mentormaya.com>
2022-08-08 17:07:22 +0545

    pyinstaller updated

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,3 +1,3 @@
 echo off
 echo "Building the project..."
-pyinstaller --onefile --windowed --icon "assets//icon.ico" --name "MaYa_Utility" --resource "assets" --resource "uploads" main.py
+pyinstaller --onefile --windowed --icon "assets//icon.ico" --name "MaYa_Utility" --clean --resource "assets" --resource "uploads" --add-data ".venv/Lib/site-packages/nepali_datetime/data/calendar_bs.csv;nepali_datetime/data/" main.py
