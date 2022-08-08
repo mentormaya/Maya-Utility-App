5aa966c013fa54ea5b412dc276909f1c86adaab6
Ajay Singh <ajay@mentormaya.com>
2022-08-08 16:27:48 +0545

    pyinstaller fixing

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -1,10 +1,3 @@
-echo "Building the project..."
 echo off
-pyinstaller ^
-    --onefile ^
-    --windowed ^
-    --icon "assets//icon.ico" ^
-    --name "MaYa_Utility" ^
-    --resource "assets" ^
-    --resource "uploads" ^
-    main.py
+echo "Building the project..."
+pyinstaller --onefile --windowed --icon "assets//icon.ico" --name "MaYa_Utility" --resource "assets" --resource "uploads" main.py
